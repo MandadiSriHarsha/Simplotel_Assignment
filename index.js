e871d61554ae623cf57c499f1cfc9eed5c6d86e4
one_bit_remmargorp <117906491+MandadiSriHarsha@users.noreply.github.com>
2024-07-24 23:23:12 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <script src="https://kit.fontawesome.com/8becc22c25.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <nav class="menubar" id="navbar">
+        <div class="menu-items-container">
+            <a href="https://www.simplotel.com/" target="_blank" class="menu-item">Home</a>
+            <a href="https://www.simplotel.com/products/central-reservation-office.html" target="_blank" class="menu-item">Order</a>
+            <a href="https://www.swiggy.com/" target="_blank" class="menu-item">Food</a>
+            <a href="https://www.simplotel.com/products/hotel-website-builder-and-booking-engine.html" target="_blank" class="menu-item">Restaurant</a>
+            <a href="https://www.simplotel.com/success-stories.html" target="_blank" class="menu-item">Testimonials</a>
+            <a a href="https://www.simplotel.com/products.html" target="_blank" class="menu-item">Contact Us</a>
+        </div>
+        <div class="icons-bg-container">
+            <a href="https://www.simplotel.com/contact-us.html#contact-simplotel" target="_blank" class="icon-button mobile-icon">
+                <i class="fa-solid fa-phone"></i>
+            </a>
+            <a href="https://www.simplotel.com/products.html" target="_blank" class="icon-button">
+                <i class="fa-solid fa-cart-shopping"></i>
+            </a>
+        </div>
+    </nav>
+    <div class="home-page-bg-container" id="homePage">
+        <div class="home-page-content-card">
+            <h1 class="home-page-content-card-heading">LOREM IPSUM</h1>
+            <p class="home-page-content-card-description">
+                Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took
+                a galley of type and scrambled it to make a type
+                specimen book.
+            </p>
+        </div>
+    </div>
+    <div class="order-page-bg-container" id="orderPage">
+        <h1 class="order-page-main-heading">Order Now</h1>
+        <div class="orders-bg-container">
+            <div class="order-card">
+                <img src="https://res.cloudinary.com/dqqgljlsw/image/upload/v1721833825/img4_dnqkpp.jpg" class="order-image" />
+                <h1 class="order-heading">BURGER</h1>
+                <p class="order-description">
+                    Lorem Ipsum has been the
+                    industry's standard dummy text
+                </p>
+            </div>
+            <div class="order-card">
+                <img src="https://res.cloudinary.com/dqqgljlsw/image/upload/v1721833805/img2_q8naun.jpg" class="order-image" />
+                <h1 class="order-heading">PIZZA</h1>
+                <p class="order-description">
+                    Lorem Ipsum has been the
+                    industry's standard dummy text
+                </p>
+            </div>
+            <div class="order-card">
+                <img src="https://res.cloudinary.com/dqqgljlsw/image/upload/v1721833816/img3_vznmno.jpg" class="order-image" />
+                <h1 class="order-heading">PASTA</h1>
+                <p class="order-description">
+                    Lorem Ipsum has been the
+                    industry's standard dummy text
+                </p>
+            </div>
+        </div>
+        <div style="text-align:center;">
+            <button class="order-now-button">
+                Order
+            </button>
+        </div>
+    </div>
+</body>
+
+</html>
